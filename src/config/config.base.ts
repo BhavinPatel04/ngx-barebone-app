c5aa831f4942086dcf30af8ebaf30a5436630708
Vatsal Pande <vpande@ebay.com>
2019-01-11 19:21:43 -0800

    Removed un necessary config

--- src/config/config.base.ts
+++ src/config/config.base.ts
@@ -4,53 +4,14 @@
  */
 export const Config = {
   production: false,
-
-  name: "Harvest",
-  title: "Harvest",
-
+  name: "SampleApp",
+  title: "SampleApp",
   uri: {
     api: "api"
   },
-
   default: {
     displayCount: 20
   },
-
-  piwik: {
-    enabled: false,
-    app: "appName",
-    prod: "prodName",
-    url: "",
-    siteId: 0
-  },
-
-  intro: {
-    enabled: false
-  },
-
-  SSO: {
-    enabled: true,
-    type: "PFSSO", // read user info from header
-    // type: 'cookie',             // read user info from cookie
-
-    // cookie will still be a fall back of PFSSO when type is `PFSSO`
-    cookieKeys: ["ihub-user"], // cookies will read
-    cookieTransform: (values) => {
-      if (values[0]) {
-        const objs = JSON.parse(decodeURIComponent(values[0]));
-        const user = objs.reduce((a, d) => Object.assign(a, d));
-        return user;
-      }
-      return null;
-    }
-  },
-
-  // sentry support
-  raven: {
-    enabled: false,
-    url: ""
-  },
-
   urlHtml5Mode: true,
   version: "1.0.0"
 };
